1af00082369e164e815c7cdfde65243ec8883875
Matheus <128961347+W-String@users.noreply.github.com>
2025-04-08 04:35:34 +0000

    first changes

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //3. Create the procedures (functions) that will draw a random card
+      //3a. Declaring and assigning drawnValue and drawnSuit variables
+      //3b. Assign them to classes in the HTML using querySelector
+      //3c. Take into consideration suit color (conditional)
+      //3d. Display the card on the screen
+
+
+      const displayCard = () =>
+        let displayCard = document.querySelector('.card');
+        console.log(displayedCard)
+  }
+
+
+displayCard()
 };
